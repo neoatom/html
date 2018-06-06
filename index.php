00ab0bf71a071b9943101d8b412e9400e7914696
neoatom <neoatom2k17@gmail.com>
2018-06-07 01:19:08 +0800

    added and made some changes

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <title>EdLabs | PHP Home</title>
+  <link rel="stylesheet" href="css/styles.css">
+ </head>
+ <body>
+
+  <h1>My first PHP page</h1>
+  
+   <?php
+    echo "Hello World!";
+   ?>
+
+ </body>
+</html>
